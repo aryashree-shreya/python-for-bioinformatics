c7f04edba1119a3dd883779c0fd6349db969a0d6
aryashree-shreya <shreya.aryashree123@gmail.com>
2026-07-15 18:21:48 +0530

    Data types in python using biological examples

--- basics/data_structures.py
+++ basics/data_structures.py
@@ -1,1 +1,16 @@
-
+#data structures in python
+#Author Shreya Aryashree
+gene_name="BRCA1"
+gene_id=672
+gc_content=42.7
+organism="Homo sapiens"
+gene_type="protein coding"
+print(gene_name)
+print(gene_id)
+print(organism)
+print(gene_type)
+print("Gene name",type(gene_name))
+print("Gene id",type(gene_id))
+print("GC content",type(gc_content))
+print("Organism",type(organism))
+print("gene type",type(gene_type))
