31f87f945929400d14bbef738b23c5083594de45
aryashree-shreya <shreya.aryashree123@gmail.com>
2026-07-15 18:09:13 +0530

    variables in python using biological eamples

--- basics/variables.py
+++ basics/variables.py
@@ -1,1 +1,9 @@
-
+# variables in python
+# Author Shreya Aryashree
+#variables in python using biological eamples
+gene_name="BRCA1"
+gene_id=672
+organism="Homo sapiens"
+print(gene_name)
+print(gene_id)
+print(organism)
