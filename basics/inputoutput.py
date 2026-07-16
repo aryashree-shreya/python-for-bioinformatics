f55a96be947949ff3601920f3384f97f77906a10
aryashree-shreya <shreya.aryashree123@gmail.com>
2026-07-16 09:38:07 +0530

    input output

--- basics/inputoutput.py
+++ basics/inputoutput.py
@@ -1,2 +1,11 @@
 #input_output in python
 #Author Shreya Aryashree
+gene_name=input("Enter gene name")
+organism_name=input("Enter organism name")
+location_chr=int(input("Enter chromosome location"))
+dna_seq=input("Enter DNA sequence")
+print("Gene name:",gene_name)
+print("Organism_name",organism_name)
+print("Location of chromosome",location_chr)
+print("Length of dna seq",len(dna_seq))
+
