aa061d3f0de62143837a981668ed3cc699be073a
aryashree-shreya <shreya.aryashree123@gmail.com>
2026-07-16 09:55:47 +0530

    operators biology

--- basics/operators.py
+++ basics/operators.py
@@ -1,1 +1,11 @@
+#operators in python using biological examples
+#Author Shreya Aryashree
+seq1=1235
+seq2=4657
+total= seq1+seq2
+sub=seq2-seq1
+avg=(seq1+seq2)/2
+rem=seq2/seq1
+div=seq2/seq1
+print(total,sub,avg,rem,div)
 
